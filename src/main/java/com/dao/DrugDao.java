ce821888e0e49bf12da4ee49a53b998a584530cd
wxwhowever <1799197002@qq.com>
2018-03-07 11:31:25 +0800

    XJY

---
+++ src/main/java/com/dao/DrugDao.java
@@ -0,0 +1,72 @@
+package com.dao;
+
+import com.base.BaseDao;
+import com.entity.Drug;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+
+public interface DrugDao extends BaseDao<Drug> {
+    /**
+     * 查询所有的集合 + 分页
+     * @param sql
+     * @param begin
+     * @param end
+     * @return
+     */
+    Map<String, Object> queryMap(String sql, int begin, int end);
+
+    /**
+     * 查询所有不带参
+     * @return
+     */
+    List<Drug> queryList();
+
+    /**
+     * 查询所有带参
+     * @return
+     */
+    public List<Drug> queryLists(String sql, int begin, int end);
+
+    /**
+     * 查询单个
+     * @param serializable
+     * @return
+     */
+    Drug queryById(Serializable serializable);
+
+    /**
+     * 查询最大的编号
+     * @return
+     */
+    public String queryMaxNo();
+
+    /**
+     * 增加
+     * @param object
+     * @return
+     */
+    boolean insert(Object object);
+
+    /**
+     * 修改
+     * @param object
+     * @return
+     */
+    boolean update(Object object);
+
+    /**
+     * 删除
+     * @param object
+     * @return
+     */
+    boolean delete(Object object);
+
+    /**
+     * 查询表最大行数
+     * @param sql
+     * @return
+     */
+    int getCount(String sql);
+}
